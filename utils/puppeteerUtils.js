3f0455db10c9060d8c577b26439ee8f564d66efe
Víctor <Victor@MacBook-Air-de-Victor.local>
2023-12-31 17:15:07 +0100

    Set timeout page navigation

--- utils/puppeteerUtils.js
+++ utils/puppeteerUtils.js
@@ -8,7 +8,10 @@ export const launchPuppeteer = async () => {
     args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
     executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
     headless: "true",
-    timeout: 600000// Otras configuraciones...
+    timeout: 600000,
+    protocolTimeout: 300000,
+    defaultViewport: null,
+    ignoreHTTPSErrors: true// Otras configuraciones...
   });
 
   return browser;
