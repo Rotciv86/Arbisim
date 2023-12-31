7761ba9b3b89d858fb7bf7ad41b58538644504dc
Víctor <Victor@MacBook-Air-de-Victor.local>
2023-12-31 14:40:53 +0100

    Increase browser timeout to 10 minutes

--- utils/puppeteerUtils.js
+++ utils/puppeteerUtils.js
@@ -8,7 +8,7 @@ export const launchPuppeteer = async () => {
     args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
     executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
     headless: "true",
-    timeout: 300000// Otras configuraciones...
+    timeout: 600000// Otras configuraciones...
   });
 
   return browser;
